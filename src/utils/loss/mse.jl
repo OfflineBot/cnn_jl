8641187b6bb17d841021f4526909d9df1c266853
OfflineBot <flintstonelg@gmail.com>
2025-07-21 15:44:16 +0200

    mse divide by length

--- src/utils/loss/mse.jl
+++ src/utils/loss/mse.jl
@@ -1,7 +1,7 @@
 
 
 mse(pred::Matrix{Float32}, truth::Matrix{Float32})::Float32 =
-    sum((pred .- truth).^2) * length(pred)
+    sum((pred .- truth).^2) / length(pred)
 
 mse_backward(pred::Matrix{Float32}, truth::Matrix{Float32})::Matrix{Float32} =
     pred .- truth
